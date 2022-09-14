73da780e0dc341ca0094c98bf20aec140b8c5642
thatmp7 <87043765+thatmp7@users.noreply.github.com>
2022-09-14 17:11:10 -0400

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "ip_sets_arn"{
+    description = "Array containing the full resource object and attributes for all ip sets created"
+    value = aws_wafv2_ip_set.this.arn
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "create" {
+  description = "Determines whether resources will be created"
+  type        = bool
+  default     = true
+}
+
+variable "ip_set" {
+  description = "A map of interface and/or gateway endpoints containing their properties and configurations"
+  type        = any
+  default     = {}
+}
+
+variable "ip_set_addresses" {
+  description = "Default security group IDs to associate with the VPC endpoints"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to use on all resources"
+  type        = map(string)
+  default     = {}
+}
